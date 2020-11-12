de040a50209052afca7b0d18be03647efa85083c
Ennouinou Alae <alaeddinne.ennouinou@gmail.com>
2020-11-12 15:32:39 +0100

    create service interface : publication of services

---
+++ src/org/mql/jee/business/CompanyService.java
@@ -0,0 +1,17 @@
+package org.mql.jee.business;
+
+import java.util.List;
+
+import org.mql.jee.entities.City;
+import org.mql.jee.entities.Company;
+
+public interface CompanyService {
+	public int add(City city);
+	public int add(Company company);
+	public City getCity(int id);
+	public Company geCompany(int id);
+	public List<City> getCities();
+	public List<Company> getCompanies();
+	public List<City> getCities(int limit);
+	public List<Company> getCompanies(int limit);
+}
